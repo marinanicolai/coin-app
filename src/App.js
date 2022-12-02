851d9593e6e9c64f7d34e9b51a942d3239c4220e
marina <nicolaimarina@gmail.com>
2022-12-02 12:36:18 -0500

    adding the searhc component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 import CoinList from "./components/CoinList/CoinList";
 import NavBar from "./components/NavBar/NavBar";
+import { searchContext } from "./Context/SearchContext";
 
 function App() {
+  const [search, setSearch] = useState({});
+  const [allRecodrs, setRecords] = useState({});
   return (
-    <div className="Cryoto">
-      <h1>Crypto App</h1>
-      <NavBar />
+    <searchContext.Provider
+      value={{ search, setSearch, allRecodrs, setRecords }}
+    >
+      <div className="Cryoto">
+        <h1>Crypto App</h1>
+        <NavBar />
 
-      <CoinList />
-    </div>
+        <CoinList />
+      </div>
+    </searchContext.Provider>
   );
 }
 
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,39 @@
-import React from "react";
+import { React, useContext, useEffect, useState } from "react";
+import { searchContext } from "../../Context/SearchContext";
+import axios from "axios";
 
 const Search = () => {
+  const { search, setSearch, allRecords, setRecords } =
+    useContext(searchContext);
+
+  const [input, setInput] = useState("");
+
+  const searchRecord = (input) => {
+    console.log(input);
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/${input}?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setSearch(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+      )
+      .then((res) => {
+        setRecords(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(search.name);
+  console.log(allRecords);
+
   return (
     <div>
       <input />
