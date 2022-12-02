89306d0491817bc8e83fe95e9235680a5a8151e9
marina <nicolaimarina@gmail.com>
2022-12-02 11:53:57 -0500

    adding the Context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import CoinList from "./components/CoinList/CoinList";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="Cryoto">
       <h1>Crypto App</h1>
+      <NavBar />
+
       <CoinList />
     </div>
   );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../Search/Search";
+
+const NavBar = () => {
+  return (
+    <div>
+      <h3>NavBar</h3>
+      <Search />
+    </div>
+  );
+};
+
+export default NavBar;
